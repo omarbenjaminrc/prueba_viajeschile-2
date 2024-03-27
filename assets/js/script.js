8716de87e18bd2037d313a5a2ec574fd7938fabe
unknown <omarbenjaminrc@gmail.com>
2024-03-27 16:00:53 -0300

    modifiqué las imagenes de testimonios, el botón del formulario y agregué jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,9 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
 // alert button
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Tu mensaje ha sido enviado correctamente', 'success')
-  })
-}
-
+$( "#enviarCorreo" ).on( "click", function() {
+  alert( "El correo fue enviado correctamente." );
+} );
 
 
 
