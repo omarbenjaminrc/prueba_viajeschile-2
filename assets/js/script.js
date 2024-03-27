fbc2e5087ee71e08cc5c87a6d5bad1909ed8fbd4
unknown <omarbenjaminrc@gmail.com>
2024-03-27 16:32:46 -0300

    cambié el boton y borré jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,26 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
 // alert button
-$( "#enviarCorreo" ).on( "click", function() {
-  alert( "El correo fue enviado correctamente." );
-} );
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Tu mensaje ha sido enviado correctamente', 'success')
+  })
+}
+
 
 
 
